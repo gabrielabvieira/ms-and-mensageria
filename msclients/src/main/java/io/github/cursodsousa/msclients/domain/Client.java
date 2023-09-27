7e85b311c197dec1a1cc67af76b7e69beda52f3e
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-27 12:36:44 -0300

    feat: adding Jpa dependency

---
+++ msclients/src/main/java/io/github/cursodsousa/msclients/domain/Client.java
@@ -0,0 +1,2 @@
+package io.github.cursodsousa.msclients.domain;public class Client {
+}
---
+++ msclients/src/main/java/io/github/cursodsousa/msclients/infra/repository/ClientRepository.java
@@ -0,0 +1,2 @@
+package io.github.cursodsousa.msclients.infra.repository;public interface ClientRepository {
+}
