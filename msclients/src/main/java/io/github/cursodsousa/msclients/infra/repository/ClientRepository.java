08870a8c83f14cb330e0c0f5e2f6317e233644a8
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-27 12:40:18 -0300

    feat: entity and repository added

--- msclients/src/main/java/io/github/cursodsousa/msclients/infra/repository/ClientRepository.java
+++ msclients/src/main/java/io/github/cursodsousa/msclients/infra/repository/ClientRepository.java
@@ -1,2 +1,7 @@
-package io.github.cursodsousa.msclients.infra.repository;public interface ClientRepository {
+package io.github.cursodsousa.msclients.infra.repository;
+
+import io.github.cursodsousa.msclients.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
