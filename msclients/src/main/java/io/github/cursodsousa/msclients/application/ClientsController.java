bb0eae6163e6256d5dab6162abd09da254123570
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-27 15:47:08 -0300

    feat: request where to save the created customer data

--- msclients/src/main/java/io/github/cursodsousa/msclients/application/ClientsController.java
+++ msclients/src/main/java/io/github/cursodsousa/msclients/application/ClientsController.java
@@ -1,15 +1,43 @@
 package io.github.cursodsousa.msclients.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.cursodsousa.msclients.application.representation.ClientSaveRequest;
+import io.github.cursodsousa.msclients.domain.Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientsController {
+
+    private final ClientService service;
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClientSaveRequest request) {
+       var client  = request.toModel();
+       service.save(client);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(client.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dataClient(@RequestParam String cpf){
+        var client = service.getByCPF(cpf);
+        if(client.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(client);
     }
 }
