b160e66276fbf63f3099c36f42099adbdb3f5cf9
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-27 23:53:08 -0300

    obtaining microservice status from clients and load balance

--- msclients/src/main/java/io/github/cursodsousa/msclients/application/ClientsController.java
+++ msclients/src/main/java/io/github/cursodsousa/msclients/application/ClientsController.java
@@ -3,6 +3,7 @@ package io.github.cursodsousa.msclients.application;
 import io.github.cursodsousa.msclients.application.representation.ClientSaveRequest;
 import io.github.cursodsousa.msclients.domain.Client;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,12 +13,14 @@ import java.net.URI;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientsController {
 
     private final ClientService service;
 
     @GetMapping
     public String status(){
+        log.info("Obtendo o status do microservice de clientes");
         return "ok";
     }
     @PostMapping
