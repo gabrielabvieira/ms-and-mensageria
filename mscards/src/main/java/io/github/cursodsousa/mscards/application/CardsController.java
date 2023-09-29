75b4cc593c972a12c237a304ecf440b4a2bc7b58
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-29 20:45:55 -0300

    feat: implementing endpoint that returns list of cards

--- mscards/src/main/java/io/github/cursodsousa/mscards/application/CardsController.java
+++ mscards/src/main/java/io/github/cursodsousa/mscards/application/CardsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
@@ -25,6 +27,15 @@ public class CardsController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
 
     }
+
+    @GetMapping
+    public ResponseEntity<List<Card>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
+        List<Card> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
+
+
+
 
 
 }
