42ea9c9db7a441d1fe5da94c3ba6b53c00d7f120
Gabriela <gabriela.bvieira@hotmail.com>
2023-10-01 17:08:37 -0300

    refactor: refactoring error in the name of microservice cards

--- mscloudgateway/src/main/java/io/github/cursodsousa/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/cursodsousa/mscloudgateway/MscloudgatewayApplication.java
@@ -21,7 +21,7 @@ public class MscloudgatewayApplication {
 		return builder
 				.routes()
 				    .route(r -> r.path("/clientes/**").uri("lb://msclients"))
-				    .route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
+				    .route(r -> r.path("/cartoes/**").uri("lb://mscards"))
 				.build();
 	}
 
