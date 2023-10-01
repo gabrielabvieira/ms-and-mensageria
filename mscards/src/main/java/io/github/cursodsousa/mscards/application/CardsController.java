01976f74c8ea14b2ed5af9a5847a6b83c8bc6f54
Gabriela <gabriela.bvieira@hotmail.com>
2023-10-01 16:40:53 -0300

    feat: implementing more endpoints

--- mscards/src/main/java/io/github/cursodsousa/mscards/application/CardsController.java
+++ mscards/src/main/java/io/github/cursodsousa/mscards/application/CardsController.java
@@ -1,20 +1,24 @@
 package io.github.cursodsousa.mscards.application;
 
 import io.github.cursodsousa.mscards.application.dto.CardSaveRequest;
+import io.github.cursodsousa.mscards.application.representation.CartoesPorClienteResponse;
 import io.github.cursodsousa.mscards.domain.Card;
+import io.github.cursodsousa.mscards.domain.ClientCard;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
 public class CardsController {
 
-    private final CardService service;
+    private final CardService cardService;
+    private final ClientCardService clientCardService;
 
     @GetMapping
     public String status(){
@@ -23,19 +27,25 @@ public class CardsController {
     @PostMapping
     public ResponseEntity cadastra(@RequestBody CardSaveRequest request){
         Card card = request.toModel();
-        service.save(card);
+        cardService.save(card);
         return ResponseEntity.status(HttpStatus.CREATED).build();
 
     }
 
-    @GetMapping
+    @GetMapping(params = "renda")
     public ResponseEntity<List<Card>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
-        List<Card> list = service.getCartoesRendaMenorIgual(renda);
+        List<Card> list = cardService.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok(list);
     }
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(
+            @RequestParam("cpf") String cpf){
+        List<ClientCard> lista = clientCardService.listCartoesByCpf(cpf);
+        List<CartoesPorClienteResponse> resultList = lista.stream()
+                .map((CartoesPorClienteResponse::fromModel))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(resultList);
 
-
-
-
+    }
 
 }
