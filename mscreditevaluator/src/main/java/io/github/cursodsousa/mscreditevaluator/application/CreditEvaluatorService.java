8549ae3d5d070c2f8c36092867ee2b51936d9fff
Gabriela <gabriela.bvieira@hotmail.com>
2023-10-02 14:19:14 -0300

    feat: implementation for obtaining customer cards (mscards)

--- mscreditevaluator/src/main/java/io/github/cursodsousa/mscreditevaluator/application/CreditEvaluatorService.java
+++ mscreditevaluator/src/main/java/io/github/cursodsousa/mscreditevaluator/application/CreditEvaluatorService.java
@@ -1,27 +1,34 @@
 package io.github.cursodsousa.mscreditevaluator.application;
 
+import io.github.cursodsousa.mscreditevaluator.domain.model.ClientCard;
 import io.github.cursodsousa.mscreditevaluator.domain.model.ClientSituation;
 import io.github.cursodsousa.mscreditevaluator.domain.model.DataClient;
+import io.github.cursodsousa.mscreditevaluator.infra.clients.CardsControllerClient;
 import io.github.cursodsousa.mscreditevaluator.infra.clients.ClientControllerClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CreditEvaluatorService {
 
     private final ClientControllerClient clientesClient;
+    private final CardsControllerClient cartoesClient;
 
     public ClientSituation obterClientSituation(String cpf){
         // obterDadosCliente -MSCLIENTS
         // obter cartoes do cliente -MSCARTOES
 
         ResponseEntity<DataClient> dataClientResponse = clientesClient.dataClient(cpf);
+        ResponseEntity<List<ClientCard>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
         return ClientSituation
                 .builder()
                 .cliente(dataClientResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
 
     }
