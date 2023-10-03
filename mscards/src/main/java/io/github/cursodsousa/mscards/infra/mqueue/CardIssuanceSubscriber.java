80680e5868a3c4a0c4a6c989cfe75149343ef765
Gabriela <gabriela.bvieira@hotmail.com>
2023-10-03 20:20:43 -0300

    feat: Implementation of the RabbitMQ card issuing queue in the cards microservice

--- mscards/src/main/java/io/github/cursodsousa/mscards/infra/mqueue/CardIssuanceSubscriber.java
+++ mscards/src/main/java/io/github/cursodsousa/mscards/infra/mqueue/CardIssuanceSubscriber.java
@@ -1,2 +1,14 @@
-package io.github.cursodsousa.mscards.infra.mqueue;public class CardIssuanceSubscriber {
+package io.github.cursodsousa.mscards.infra.mqueue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardIssuanceSubscriber {
+
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receiveRequestIssue(@Payload String payload){
+        System.out.println(payload);
+    }
 }
