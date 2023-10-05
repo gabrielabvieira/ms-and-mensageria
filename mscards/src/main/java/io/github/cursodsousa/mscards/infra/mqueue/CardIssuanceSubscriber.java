15f33e5350c89b779ecfcf25c08134d359dbb5a4
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-05 14:13:49 -0300

    feat: adding log to the card microservice

--- mscards/src/main/java/io/github/cursodsousa/mscards/infra/mqueue/CardIssuanceSubscriber.java
+++ mscards/src/main/java/io/github/cursodsousa/mscards/infra/mqueue/CardIssuanceSubscriber.java
@@ -8,12 +8,14 @@ import io.github.cursodsousa.mscards.domain.ClientCard;
 import io.github.cursodsousa.mscards.infra.repository.CardRepository;
 import io.github.cursodsousa.mscards.infra.repository.ClientCardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CardIssuanceSubscriber {
 
     private final CardRepository cardRepository;
@@ -33,7 +35,7 @@ public class CardIssuanceSubscriber {
             clientCardRepository.save(clientCard);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
 
         }
     }
