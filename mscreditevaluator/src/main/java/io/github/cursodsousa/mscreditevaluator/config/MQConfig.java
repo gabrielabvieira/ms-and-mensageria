21f9829476dce7e0a98419e79ae2b237dc0898d9
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-04 22:08:38 -0300

    feat: adding @Bean to MQueue for registration in the container

--- mscreditevaluator/src/main/java/io/github/cursodsousa/mscreditevaluator/config/MQConfig.java
+++ mscreditevaluator/src/main/java/io/github/cursodsousa/mscreditevaluator/config/MQConfig.java
@@ -2,6 +2,7 @@ package io.github.cursodsousa.mscreditevaluator.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,6 +11,7 @@ public class MQConfig {
     @Value("${mq.queues.emissao-cartoes}")
     private String cardIssuanceQueue;
 
+    @Bean
     public Queue queueCardIssuance(){
         return new Queue(cardIssuanceQueue, true);
     }
